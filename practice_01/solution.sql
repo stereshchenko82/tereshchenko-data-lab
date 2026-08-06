59197dc0a18339193a1ad0bfe61daed7594ce9c9
stereshchenko82 <terikon1982@gmail.com>
2026-08-06 12:41:05 +0300

    відкореговано помилки

--- practice_01/solution.sql
+++ practice_01/solution.sql
@@ -2,11 +2,11 @@ CREATE TABLE students (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     grade INT NOT NULL
-)
+);
 
 INSERT INTO students (name, grade) VALUES
 ('Олена Коваль', 92),
 ('Іван Петренко', 85),
-('Марія Шевченко', 78)
+('Марія Шевченко', 78);
 
-SELECT * FROM students
+SELECT * FROM students;
